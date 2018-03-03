852c6486927dd88c9e3aa8d9385364b3e97619ab
Willians Martins <professorwillians@gmail.com>
2018-03-03 10:08:06 -0300

    add arquivo da aula3.js

---
+++ js/aula1.js
@@ -0,0 +1,6 @@
+var nome = "Yan Gomes"
+
+console.info("nome : " + nome);
+console.error("nome : " + nome);
+console.log("nome : " + nome);
+document.write("nome : " + nome);
---
+++ js/aula3.js
@@ -0,0 +1,25 @@
+//declaracao
+var idade;
+
+//atribuicao 1
+idade = 18;
+console.info( idade );
+
+//atribuicao 2
+idade = 20;
+console.info( idade );
+
+idade += 5; //idade = idade + 5;
+console.info( idade );
+
+idade -= 5; //idade = idade - 5;
+console.info( idade );
+
+idade *= 5; //idade = idade * 5;
+console.info( idade );
+
+idade /= 5; //idade = idade / 5;
+console.info( idade );
+
+idade %= 5; //idade = idade % 5;
+console.info( idade );
