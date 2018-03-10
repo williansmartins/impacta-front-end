67b2ac440f5161a2d362b5db780ae4c016044dc9
Administrador Local <LocalAdmin@sala218.classroom.net>
2018-03-10 14:55:16 -0300

    evolucao

--- js/aula4.js
+++ js/aula4.js
@@ -1,5 +1,19 @@
 
 
 $(document).ready(function(){
-	alert("estou pronto");
+	$.ajax({
+	  	url: "http://sistema.memphisnet.com.br/barramento/api/v1/banner",
+	}).done(function(response) {
+	  	//alert(response);
+	});
+
+	for (var i = 1; i <= 5; i++) {
+		var conteudo = `
+			<tr>
+				<td>coluna1</td>
+				<td>coluna2</td>
+			</tr>`;
+		$("#tabela").append(conteudo);
+	}
+
 });
