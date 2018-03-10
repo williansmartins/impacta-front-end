1300e94554b0ca69658879f7573f094dc04aef84
Administrador Local <LocalAdmin@sala218.classroom.net>
2018-03-10 13:48:00 -0300

    add numeros primos

---
+++ js/primos.js
@@ -0,0 +1,24 @@
+
+function verificarSeEhPrimo(entrada){
+	var variavel = entrada;
+	var verificador = 0;
+
+
+	for(contador = variavel-1; contador>=2; contador--){ 
+
+		if(variavel % contador == 0){
+			//nao primo
+			verificador++;
+		}
+	}
+
+	if(verificador==0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+for(i=0; i<=100; i++){
+	console.info("i="+i + "->" +verificarSeEhPrimo(i));
+}
