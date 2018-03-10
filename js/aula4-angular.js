7cd3dc70a744da8d3205b3e4fa3628edf3308e2a
sasala21802@CLASSROOM.NET <sasala21802@CLASSROOM.NET>
2018-03-10 17:55:53 -0300

    conexão com senha

--- js/aula4-angular.js
+++ js/aula4-angular.js
@@ -9,4 +9,42 @@ app.controller('bannerCtrl', function($scope, $http) {
 	    });
 	}
 
+	$scope.buscar2 = function(){
+
+ 			$http({
+                method : "GET",
+                headers: {
+			        'Content-Type': 'application/json',
+			        'X-VTEX-API-AppKey': 'xxx',
+			        'X-VTEX-API-AppToken': 'xxx'
+			    },
+                url : 'http://xxx.xxx.com.br/api/catalog_system/pvt/category/xxx',
+            }).then(function (xxx) {
+			    // success function
+			    alert("sucesso");
+			}, function (xxx) {
+			    // Failure Function
+			    alert("erro");
+			});
+
+
+		// var req = {
+		//     method: 'GET',
+		//     url: 'http://xxx.xxx.com.br/api/catalog_system/pvt/category/xxx',
+		//     headers: {
+		//         'Content-Type': 'application/json',
+		//         'X-VTEX-API-AppKey': 'xxx',
+		//         'X-VTEX-API-AppToken': 'xxx'
+		//     }
+		// }
+
+		// $http(req).then(function (responce) {
+		//     // success function
+		//     alert("sucesso");
+		// }, function (responce) {
+		//     // Failure Function
+		//     alert("erro");
+		// });
+	}
+
 });
